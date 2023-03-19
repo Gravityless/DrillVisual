eb8b398692fb4c0127a5255c6455d757c2fefada
naichamilktea <78580403+naichamilktea@users.noreply.github.com>
2023-03-19 23:07:21 +0800

    更改显示数据

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 钻孔数据可视化项目
 ## 使用方法：
->  1.更改Geoserver参数(在src/main/webapp/js/mapjs的第7 8行，改变端口号和图层)<br>2.在idea中下载Maven Helper插件，然后右键项目->Run Maven->Tomcat7:run即可，按照提示打开网站 <br> 
+>  1.更改Geoserver参数(在src/main/webapp/js/mapjs的第7 8行，改变端口号和图层)<br>2. 将数据库导出为.shp文件，并转换投影为UTM投影，否则leaflet无法识别<br>3.在idea中下载Maven Helper插件，然后右键项目->Run Maven->Tomcat7:run即可，按照提示打开网站 <br> 
--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,11 +1,11 @@
 
 var leafletMap = new L.Map("mapDiv", {
-    zoom: 15,
-    center: [29.566701, 115.986335],
+    zoom: 18,
+    center: [32.216311,119.443884],
     boxZoom: true,
 });
 var wmsLayer= L.tileLayer.wms("http://localhost:5436/geoserver/lmy/wms", { // 链接要改对应的
-    layers: 'lmy:gf2_roi',//需要加载的图层,就是我们刚刚新建的。map 和 图层名称
+    layers: 'lmy:drillpoints_wgs84',//需要加载的图层,就是我们刚刚新建的。map 和 图层名称
 });
 leafletMap.addLayer(wmsLayer);
 
