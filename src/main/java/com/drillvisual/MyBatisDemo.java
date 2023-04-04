e8df704e343e29c13837aa061b26f36df99ffeec
SonicoGO <song.xv@outlook.com>
2023-04-04 16:20:56 +0800

    connected to mssql successfully

---
+++ src/main/java/com/drillvisual/MyBatisDemo.java
@@ -0,0 +1,27 @@
+package com.drillvisual;
+
+import java.util.List;
+import com.drillvisual.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisDemo {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        List<User> users = sqlSession.selectList("user.selectAll");
+
+        System.out.println(users);
+
+        sqlSession.close();
+    }
+}
