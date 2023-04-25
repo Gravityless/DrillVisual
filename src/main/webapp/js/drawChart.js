a3393ed411d96e377aab18d165b2de889b03ec49
SonicoGO <song.xv@outlook.com>
2023-04-25 10:45:43 +0800

    edit drawChart.js

--- src/main/webapp/js/drawChart.js
+++ src/main/webapp/js/drawChart.js
@@ -1,156 +1,120 @@
-// 这里输入模拟JSON数组
-request = ["35-1001","35-1002","35-1003"]
+// POST
+function plotcht(request){
+    //封装成函数，方便传入request参数
+    axios.post('http://localhost/DrillVisual/ChartPlot', request)
+        .then(function (response) {
+            console.log("response from server:"+response)
+            // 这里获取返回数据
+            // document.getElementById("output").innerHTML = JSON.stringify(response.data);
+            var result = response.data;
+            document.querySelector("#dataDiv").removeAttribute('_echarts_instance_')
+            // 获取图表元素
+            var myChart = echarts.init(document.getElementById("dataDiv"));
+            // 指定数据xArray, seriesArray, legendArray
+            var xArray = [];
+            var seriesArray = [];
+            var legendArray = [];
+            //比较两个点是否重合
+            function comparepoint(x,y){
+                return Math.abs(x[0]-y[0])<1e-6 && Math.abs(x[1]-y[1])<1e-6
+            }
+            // 计算xArray
+            var idx, sum;
+            sum = 0;
+            for (idx = 0; idx < result.drillDistance.length; idx++) {
+                xArray[idx] = sum;
+                sum += result.drillDistance[idx];
+            }
+            xArray[idx] = sum;
 
-var chart1 = echarts.init(document.getElementById("dataDiv"));
-// 指定图表的配置项和数据
-var dataX = ['18~30', '31~40', '41~50', '51~60', '61~70'];
-var dataY = ['200', '102', '422', '189', '12'];
-var dataZ = [500, 500, 500, 500, 500];
-var option = {
-    title: {
-        text: '钻孔数据分布',
-        link: 'https://blog.csdn.net/gray_key',
-        target: 'blank',
-        left: '5%',
-        textStyle: {
-            color: '#fff',
-            fontSize: 15,
-            bottom: 20
-        }
-    },
-    grid: {
-        left: 40,
-        right: 40,
-        top: 30,
-        bottom: 25,
-    },
-    backgroundColor: 'rgba(0,0,0,0.8)',		// 背景颜色
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a}<br/>{b} : {c}人"
-    },
-    xAxis: {
-        type: 'category',
-        position: 'bottom',
-        // 等同于 axisLine: true    开始
-        axisLine: {
-            show: false
-        },
-        axisTick: {
-            show: false
-        },
-        // 等同于 axisLine: true    结束
-        axisLabel: {
-            color: '#fff',
-            fontSize: 12
-        },
-        data: dataX,
-    },
-    yAxis: {
-        splitNumber: 5, // 坐标轴的分割段数，需要注意的是这个分割段数只是个预估值，最后实际显示的段数会在这个基础上根据分割后坐标轴刻度显示的易读程度作调整。在类目轴中无效。
-        // 等同于 axisLine: true    开始
-        axisLine: {
-            show: false
-        },
-        axisTick: {
-            show: false
-        },
-        // 等同于 axisLine: true    结束
-        splitLine: {
-            show: true,
-            lineStyle: {
-                color: '#fff',
-                opacity: 0.2
+            // 计算seriesArray
+            var colNum = result.drillDistance.length + 1;
+            var arcNum = result.layerLineList.length;
+            var arcList = result.layerLineList;
+            // 连接地层线
+            var drawdat=[];//若干地层线组成的数组,每一项对应一条线，有name和data两个属性
+            for (idx = 0; idx < arcNum; idx++) {
+                var line = {
+                    data:[
+                        [xArray[arcList[idx].columnIndex], arcList[idx].depthLeft],
+                        [xArray[arcList[idx].columnIndex + 1], arcList[idx].depthRight]
+                    ],
+                    type: 'line',
+                    name: arcList[idx].stratumId,
+                    // areaStyle: {}
+                };
+                var found=false
+                for (var it=0;it<drawdat.length;++it){//将当前线段与drawdat里的同名线匹配，若能连接，则加入
+                    if (drawdat[it].name===line.name){
+                        for (var left=0;left<drawdat[it].data.length;++left){
+                            if (comparepoint(drawdat[it].data[left],line['data'][0])){
+                                if (left===drawdat[it].data.length-1){
+                                    drawdat[it].data.push(line['data'][1])
+                                    found=true
+                                    break
+                                }else{
+                                    newdat=drawdat[it];
+                                    newdat.data=drawdat[it].data.slice(0,left+1)
+                                    drawdat.push(newdat)
+                                }
+                            }
+                        }
+                    }
+                }
+                if (!found){
+                    drawdat.push({name:line.name,data:line.data})
+                }
             }
-        },
-        axisLabel: {
-            color: '#fff',
-            fontSize: 12
-        }
-    },
-    series: [{
-        name: '各年龄段人数',
-        type: 'line',
-        data: dataY,
-        smooth: true,
-        symbol: 'emptyCircle',
-        symbolSize: 5,
-        itemStyle: {
-            normal: {
-                color: '#fff'
+            // 连接钻孔线
+            drawdat.push({name:'Drill',data:[]})
+            for (idx = 0; idx < colNum; idx++) {
+                var height = result.drillPointList[idx].drillHeight;
+                var depth = result.drillPointList[idx].drillDepth;
+                var line = {
+                    data:[
+                        [xArray[idx], height],
+                        [xArray[idx], height - depth]
+                    ],
+                    type: 'line',
+                    name: 'Drill'
+                };
+                drawdat[drawdat.length-1].data.push([xArray[idx], height]) //将钻孔点push进绘制数组中，中间用-隔开，避免不同列钻孔相连
+                drawdat[drawdat.length-1].data.push([xArray[idx], height-depth])
+                drawdat[drawdat.length-1].data.push('-')
             }
-        },
-        lineStyle: {
-            normal: {
-                color: {
-                    type: 'linear',
-                    x: 0,
-                    y: 0,
-                    x2: 0,
-                    y2: 1,
-                    colorStops: [{
-                        offset: 0, color: 'rgba(255,0,0,0.8)' // 0% 处的颜色
-                    }],
-                    globalCoord: false // 缺省为 false
+
+            // 指定图表的配置项
+            var cht= {
+                chart: {
+                    type: 'line'
                 },
-                width: 3
-            }
-        },
-        areaStyle: {
-            normal: {
-                color: "rgba(51,255,255,0.3)",
-            }
-        },
-        markLine: { // 图表标线。
-            silent: false, // 图形是否不响应和触发鼠标事件，默认为 false，即响应和触发鼠标事件。
-            symbol: ["", "arrow"], // 标线两端的标记类型，可以是一个数组分别指定两端，也可以是单个统一指定，具体格式见 data.symbol。
-            // 标线的数据数组。每个数组项可以是一个两个值的数组，分别表示线的起点和终点，每一项是一个对象，有下面几种方式指定起点或终点的位置。
-            data: [{ // 直接用 type 属性标注系列中的最大值，最小值。这时候可以使用 valueIndex 或者 valueDim 指定是在哪个维度上的最大值、最小值、平均值。
-                name: '平均数',
-                type: 'average' // 支持 'average', 'min', 'max'
-            }],
-            precision: 0, // 标线数值的精度，在显示平均值线的时候有用。
-            label: { // 标线的文本。
-                normal: {
-                    formatter: '平均数:\n{c}人'
-                }
-            },
-            lineStyle: { // 标线的样式
-                normal: {
-                    color: '#fff',
-                }
-            }
-        }
-    }, {
-        name: '占位背景',
-        type: 'bar',
-        itemStyle: {
-            normal: {
-                show: true,
-                color: '#fff',
-                opacity: 0
-            }
-        },
-        silent: true, // 图形是否不响应和触发鼠标事件，默认为 false，即响应和触发鼠标事件。
-        barWidth: '50%', // 柱条宽度
-        data: dataZ, // 占位背景在y轴上的值(高度)
-        animation: false // 是否开启动画
-    }, {
-        name: '占位背景',
-        type: 'bar',
-        itemStyle: {
-            normal: {
-                show: true,
-                color: '#fff',
-                opacity: 0.1
-            }
-        },
-        silent: true,
-        barWidth: '50%',
-        barGap: 0, // 柱间距离
-        data: dataZ,
-        animation: false
-    }],
-};
-// 使用刚指定的配置项和数据显示图表
-chart1.setOption(option);
+                title: {
+                    text: '地层连线剖面图',
+                    align: 'left'
+                },
+                yAxis: {
+                    title: {
+                        useHTML: true,
+                        text: '地层深度',
+                    }
+                },
+                plotOptions: {
+                    area: {
+                        stacking: 'normal',
+                        lineColor: '#666666',
+                        lineWidth: 1,
+                        marker: {
+                            lineWidth: 1,
+                            lineColor: '#666666'
+                        }
+                    }
+                },
+                series: drawdat,
+            };
+            console.log(cht['series'])
+            $('#dataDiv').highcharts(cht);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
